672a9397a4303cf6bc79455dbb1d2de628fe82b7
Dilsha <dilsham998@gmail.com>
2021-06-30 09:31:29 +0530

    bank

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -23,28 +23,4 @@ cancel(){
     this.onCancel.emit()
   }
 }
-// import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 
-// @Component({
-//   selector: 'app-deleteconfirmatin',
-//   templateUrl: './deleteconfirmatin.component.html',
-//   styleUrls: ['./deleteconfirmatin.component.css']
-// })
-// export class DeleteconfirmatinComponent implements OnInit {
-// @Input() item : string | null | undefined
-
-// @Output() onDelete=new EventEmitter;
-
-// @Output() onCancel = new EventEmitter;
-
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-// delete(){
-//   this.onDelete.emit(this.item)
-// }
-// cancel(){
-//   this.onCancel.emit()
-// }
-// }
